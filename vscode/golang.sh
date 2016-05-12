b2a93aa0a95b5eaedb157230960e0f7a5070501a
Michel Aquino <michel.has@gmail.com>
2016-05-12 11:15:56 -0300

    Update golang.sh

--- vscode/golang.sh
+++ vscode/golang.sh
@@ -3,7 +3,7 @@
 
 go get -u github.com/golang/lint/golint
 go get -u github.com/rogpeppe/godef
-go get -u github.com/derekparker/devle/cmd/dlv
+go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/nsf/gocode
 go get -u github.com/tpng/gopkgs
 go get -u github.com/newhook/go-symbols
