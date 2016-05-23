70ff944d4b46f2ce4c54c65426b19e96e47286aa
Michel <michel@Maverick>
2016-05-22 23:39:37 -0300

    Fix golang install script

--- golang/golang.sh
+++ golang/golang.sh
@@ -7,6 +7,6 @@ fi
 echo "Set GOPATH enviroment variable"
 
 echo '' >> ~/.profile
-echo 'Golang enviroment variable' >> ~/.profile
+echo '# Golang enviroment variable' >> ~/.profile
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 echo 'export GOPATH=$HOME/golang/' >> ~/.profile
