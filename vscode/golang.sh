5c065a332a1bf7901990a4245a2dad5f774200bd
Michel Aquino <michel.has@gmail.com>
2016-05-25 00:11:24 -0300

    golang

--- vscode/golang.sh
+++ vscode/golang.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 # Get the Vscode plugins to Golang
 
+echo "Installing github.com/golang/lint/golint..."
 go get -u github.com/golang/lint/golint
+
+echo "Installing github.com/rogpeppe/godef..." 
 go get -u github.com/rogpeppe/godef
+
+echo "Installing github.com/derekparker/delve/cmd/dlv..."
 go get -u github.com/derekparker/delve/cmd/dlv
+
+echo "Installing github.com/nsf/gocode..."
 go get -u github.com/nsf/gocode
+
+echo "Installing github.com/tpng/gopkgs..."
 go get -u github.com/tpng/gopkgs
+
+echo "Installing github.com/newhook/go-symbols..."
 go get -u github.com/newhook/go-symbols
+
+echo "Installing golang.org/x/tools/cmd/godoc..."
+go get -u golang.org/x/tools/cmd/godoc
