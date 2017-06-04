fdcbd6b0d475dbd2f8125f1675a91dd809c5a1dd
Michel Aquino <michel.has@gmail.com>
2017-06-04 19:29:26 -0300

    Install Docker and Docker Compose

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -8,5 +8,25 @@ sudo apt update && apt -y upgrade
 echo "-------------------------------------"
 echo "Installing applications"
 echo "-------------------------------------"
-sudo apt-get -y install tmux code zsh
 
+echo "Installing tmux"
+sudo apt-get -y install tmux 
+
+echo "Installing VS Code"
+sudo apt-get -y install code
+
+echo "Installing zsh"
+sudo apt-get -y install zsh
+
+echo "Installing Docker"
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common \
+     docker-ce
+
+echo "Installing Docker Compose"
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
