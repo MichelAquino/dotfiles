828375ef35cca36eaf3c8deb0699ab973b17db35
Michel Aquino <michel.has@gmail.com>
2017-06-05 15:04:44 -0300

    set sudo

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -12,6 +12,9 @@ echo "-------------------------------------"
 echo "Installing curl"
 sudo apt-get -y install curl
 
+echo "Installing vim"
+sudo apt-get -y install vim
+
 echo "Installing tmux"
 sudo apt-get -y install tmux 
 
@@ -31,5 +34,7 @@ sudo apt-get install \
      docker-ce
 
 echo "Installing Docker Compose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo -i
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
