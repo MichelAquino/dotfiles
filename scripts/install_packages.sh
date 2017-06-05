623e3b519e72f099a29d48eac970c1e1e8146fb0
Michel Aquino <michel.has@gmail.com>
2017-06-05 14:57:51 -0300

    set sudo

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -31,5 +31,5 @@ sudo apt-get install \
      docker-ce
 
 echo "Installing Docker Compose"
-curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
