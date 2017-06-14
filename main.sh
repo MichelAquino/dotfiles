dbd59bf93b98453b9bbf5ba3d7ee1df003e52e8f
Michel Aquino <michel.has@gmail.com>
2017-06-14 18:14:49 -0300

    Git config

--- main.sh
+++ main.sh
@@ -2,8 +2,9 @@
 
 /bin/bash scripts/add_repositories.sh
 /bin/bash scripts/install_packages.sh
+/bin/bash scripts/install_golang.sh
 
-# /bin/bash scripts/install_golang.sh
-
+/bin/bash scripts/configure_ssh_keys.sh
+/bin/bash scripts/configure_git.sh
 /bin/bash scripts/configure_oh_my_zsh.sh
 /bin/bash scripts/configure_vscode.sh
