e708043ef232dd4d3c4b75f8aead15d9bf3f5365
Michel Aquino <michel@Maverick.lan>
2017-06-14 17:01:52 -0300

    Rename script

--- scripts/configure_oh_my_zsh.sh
+++ scripts/configure_oh_my_zsh.sh
@@ -7,4 +7,6 @@ echo "-------------------------------------"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Set oh-my-zsh config"
-mv ../zsh/zshrc ~/
+rm /home/michel/.zshrc
+cp ../zsh/zshrc /home/michel/.zshrc
+source /home/michel/.zshrc
