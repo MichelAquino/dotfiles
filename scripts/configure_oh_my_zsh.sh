329fbe7234bbd9f682ece7cc18b972937537c0f2
Michel Aquino <michel@Maverick.lan>
2017-06-14 17:33:34 -0300

    Comment

--- scripts/configure_oh_my_zsh.sh
+++ scripts/configure_oh_my_zsh.sh
@@ -6,7 +6,10 @@ echo "-------------------------------------"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "-------------------------------------"
 echo "Set oh-my-zsh config"
+echo "-------------------------------------"
+
 rm /home/michel/.zshrc
 cp ../zsh/zshrc /home/michel/.zshrc
 source /home/michel/.zshrc
