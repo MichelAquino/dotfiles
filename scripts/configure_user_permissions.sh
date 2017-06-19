8e5bd39450cb23f76f68b4a99141f31d32cb69b6
Michel Aquino <michel.has@gmail.com>
2017-06-19 20:30:00 -0300

    Fix command to add user to docker group

--- scripts/configure_user_permissions.sh
+++ scripts/configure_user_permissions.sh
@@ -3,6 +3,4 @@
 echo "-------------------------------------"
 echo "Add docker permission to $USER"
 echo "-------------------------------------"
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-newgrp docker
+sudo usermod -aG docker $USER
