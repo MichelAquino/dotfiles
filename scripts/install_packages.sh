0259c3edd45b5b9e839e178b680bcbca895f0ea1
Michel Aquino <michel.has@gmail.com>
2017-06-23 20:16:34 -0300

    Installing spotify

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -13,6 +13,7 @@ sudo apt-get -y install \
     ca-certificates \
     gnupg2 \
     software-properties-common \
+    dirmngr \
     git \
     curl \
     vim \
@@ -65,3 +66,15 @@ echo "Installing Docker"
 echo "----------------"
 sudo apt-get update
 sudo apt-get -y install docker-ce
+
+echo "----------------"
+echo "Installing Spotify"
+echo "----------------"
+wget http://ftp.br.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb -P /tmp
+sudo apt install /tmp/libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb
+
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update
+sudo apt-get install -y spotify-client
