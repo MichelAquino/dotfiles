eae161bbadd1b871ec629b12e1b4733c624d7292
Michel Aquino <michel.has@gmail.com>
2017-07-29 17:49:13 -0300

    Installing popcorn time

--- scripts/install_tools_packages.sh
+++ scripts/install_tools_packages.sh
@@ -23,3 +23,20 @@ echo "Installing Slack"
 echo "----------------"
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.6.3-amd64.deb -O /tmp/slack-desktop-2.6.3-amd64.deb
 sudo dpkg -i /tmp/slack-desktop-2.6.3-amd64.deb
+
+echo "----------------"
+echo "Installing Popcorn Time"
+echo "----------------"
+
+sudo rm -Rf /opt/popcorntime
+sudo rm -Rf /usr/bin/Popcorn-Time
+sudo rm -Rf /usr/share/applications/popcorntime.desktop
+
+wget https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Linux-64.tar.xz -O /tmp/popcorntime.tar.xz
+sudo mkdir /opt/popcorntime
+sudo tar Jxf /tmp/popcorntime.tar.xz -C /opt/popcorntime/
+
+sudo ln -sf /opt/popcorntime/Popcorn-Time /usr/bin/Popcorn-Time
+
+echo -e '[Desktop Entry]\n Version=1.0\n Name=popcorntime\n Exec=/opt/popcorntime/Popcorn-Time\n Icon=/opt/popcorntime/src/app/images/icon.png\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/popcorntime.desktop
+sudo chmod +x /usr/share/applications/popcorntime.desktop
