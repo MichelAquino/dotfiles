937c93cff2b56f6215ae7ee24debe349ac8b9795
Michel Aquino <michel.has@gmail.com>
2018-01-04 22:27:25 -0200

    Fix zsh config

--- scripts/configure_oh_my_zsh.sh
+++ scripts/configure_oh_my_zsh.sh
@@ -4,6 +4,16 @@ echo "-------------------------------------"
 echo "Set oh-my-zsh config"
 echo "-------------------------------------"
 
-rm $HOME/.zshrc
-cp zsh/zshrc $HOME/.zshrc
-source $HOME/.zshrc
+# Export env vars
+touch "$HOME/.zshrc"
+{
+    echo ''
+    echo '#### Projects'
+    echo 'export GITHUB_GO=$GOPATH/src/github.com/michelaquino'
+} >> "$HOME/.zshrc"
+
+# Set theme
+sed -i 's/"robbyrussell"/"risto"/g' ~/.zshrc
+
+# Reload
+source ~/.zshrc
