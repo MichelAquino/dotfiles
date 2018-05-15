4853c9d77d64d547d8ac5c0d9947a35cf09b9011
Michel Aquino <michel.has@gmail.com>
2018-05-14 22:42:56 -0300

    Remove unecessary file

--- main.sh
+++ main.sh
@@ -6,7 +6,6 @@
 /bin/bash scripts/install_dev_packages.sh
 /bin/bash scripts/install_tools_packages.sh
 
-/bin/bash scripts/configure_user_permissions.sh
 /bin/bash scripts/configure_ssh_keys.sh
 /bin/bash scripts/configure_git.sh
 /bin/bash scripts/configure_oh_my_zsh.sh
--- scripts/configure_user_permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "-------------------------------------"
-echo "Add docker permission to $USER"
-echo "-------------------------------------"
-sudo usermod -aG docker $USER
