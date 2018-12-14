a145df901d76fae8b109cb36067740b343ac81a5
Michel Aquino <michel.has@gmail.com>
2018-12-13 22:34:42 -0200

    update install brave

--- scripts/install_tools_packages.sh
+++ scripts/install_tools_packages.sh
@@ -11,7 +11,7 @@ echo "Installing Brave"
 echo "-------------------------------------"
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install -y brave-browser
+sudo dnf install -y brave-browser brave-keyring
 
 echo "----------------"
 echo "Installing Spotify"
