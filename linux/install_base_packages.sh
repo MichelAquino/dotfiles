5fbb97a02687b30c4ffcb940973ea445e9ffd4bb
Michel Aquino <michel.aquino@g.globo>
2021-02-22 11:27:14 -0300

    Fix tmux config

--- linux/install_base_packages.sh
+++ linux/install_base_packages.sh
@@ -11,6 +11,6 @@ echo "-------------------------------------"
 sudo apt -y install software-properties-common apt-transport-https curl ca-certificates gnupg2 
 sudo apt -y install dirmngr git vim tmux zsh terminator desktop-file-utils gvfs debian-keyring dkms openssl p7zip 
 sudo apt -y install gnupg-agent firmware-linux firmware-linux-nonfree firmware-atheros build-essential texinfo 
-sudo apt -y install libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev 
-sudo apt -y install gnutls-dev libgtk-3-dev snapd
+sudo apt -y install libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev gnome-keyring
+sudo apt -y install gnutls-dev libgtk-3-dev snapd xsel
 sudo snap install core
